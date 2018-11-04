dcc6fb536bdd150784fc638e6458f77cab32e4e1
Brandon Mitchell <brandonimitch@users.noreply.github.com>
2018-11-04 08:10:00 -0800

    Add files via upload

--- sender251.cpp
+++ sender251.cpp
@@ -22,42 +22,42 @@ using namespace std;
 int main() {
 
     // pulls from the message queue
-int qid = msgget(ftok(".",'u'), 0);
+    int qid = msgget(ftok(".",'u'), 0);
 
-// message buffer struct
-struct buf {
-    long mtype;
-    char greeting[50];
-} msg;      // buf object 'msg'
+    // message buffer struct
+    struct buf {
+        long mtype;
+        char greeting[50];
+    } msg;      // buf object 'msg'
 
-int size = sizeof(msg) - sizeof(long);
+    int size = sizeof(msg) - sizeof(long);
     
     // Last message through patch, to receiver1
     strcpy(msg.greeting, "Terminate process sender251 to receiver1");
-msg.mtype = 100;
-get_info(qid, (struct msgbuf *)&msg, size, 300);
+    msg.mtype = 100;
+    get_info(qid, (struct msgbuf *)&msg, size, 300);
     
     // Initiate Sender 251
     cout << getpid() << "Sender 251 ready!" << endl;
 	
-int totalMessages = 0;
+    int totalMessages = 0;
     
     srand(time(NULL));
     int randomNumber;
 
-while (true){
-    
-    // generate random number for message
-    do {
-        randomNumber = rand() * INT_MAX;
-    } while (randomNumber % 251 != 0);
-    
-    msgsnd(qid, (struct msgbuf *)&msg, size, 0);
-    
-    totalMessages++;
-    cout << "Message " << totalMessages << " has been sent, value sent: " << randomNumber << endl;
-    
-}
+    while (true){
+        
+        // generate random number for message
+        do {
+            randomNumber = rand() * INT_MAX;
+        } while (randomNumber % 251 != 0);
+        
+        msgsnd(qid, (struct msgbuf *)&msg, size, 0);
+        
+        totalMessages++;
+        cout << "Message " << totalMessages << " has been sent, value sent: " << randomNumber << endl;
+        
+    }
     
     // send terminating message to sender997
     msg.mtype = 700;
@@ -65,8 +65,8 @@ while (true){
     msgsnd(qid, (struct msgbuf *)&msg, size, 0);
     
     // terminate sender251
-cout << getpid() << " (Sender251): now exits" << endl;
-exit(0);
+    cout << getpid() << " (Sender251): now exits" << endl;
+    exit(0);
 }
 
 
