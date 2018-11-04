13630df28c0495c1694aaf105e2ee897736d3a34
Brandon Mitchell <brandonimitch@users.noreply.github.com>
2018-11-04 02:06:43 -0800

    Add files via upload
    Let me know what you guys think. I can work on it further tomorrow.

--- sender251.cpp
+++ sender251.cpp
@@ -3,62 +3,69 @@
 //CECS 326 - Operating Systems
 //Professor Ratana Ngo
 
-
+#include "get_info.h"
+#include <cstdio>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <sys/wait.h>
 #include <cstring>
 #include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 #include <cstdlib>
 #include <iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 
 int main() {
 
-	int qid = msgget(ftok(".",'u'), 0);	//pulls from the message queue
+    // pulls from the message queue
+	int qid = msgget(ftok(".",'u'), 0);
 
 	// message buffer struct
 	struct buf {
 		long mtype; 
-		int number; 
-	};
+		char greeting[50];
+	} msg;      // buf object 'msg'
     
-    buf msg;				
 	int size = sizeof(msg) - sizeof(long);
+    
+    // Last message to patch, to receiver1
+    strcpy(msg.greeting, "Terminate process sender251 to receiver1");
+	msg.mtype = 99;
+	get_info(qid, (struct msgbuf *)&msg, size, 300);
     
     // Initiate Sender 251
     cout << getpid() << "Sender 251 ready!" << endl;
-    
-
-	int randomNum = 251;			
-	bool status251 = false;			//boolean for while loop
-	msg.mtype = 251;			
-	msg.number = -1;			// sent with number = 99 to kill
-
-	// confirmation message when process is killed
-	get_info(qid, (struct msgbuf *)&msg, size, 251);  
 	
-	msgrcv(qid, (struct msgbuf *)&msg, size, 252, 0); //get message to begin from receiver1
+//    msgrcv(qid, (struct msgbuf *)&msg, size, 252, 0); //get message to begin from receiver1
 	int totalMessages = 0;				//counts the messages
+    
+    srand(time(NULL));
+    int randomNumber;
 
-	while(!status251){
-		msg.number = randomNum;		// send message with number divisible 251
-		
+	while (true){
+        
+        // generate random number for message
+        do {
+            randomNumber = rand() * INT_MAX;
+        } while (randomNumber % 251 != 0);
+        
 		msgsnd(qid, (struct msgbuf *)&msg, size, 0);
 		
 		totalMessages++;			//increment counter
-		cout << "Message " << totalMessages << " has been sent" << endl;
-		
-		msgrcv(qid, (struct msgbuf *)&msg, size, 252, 0); 
-		
-		// stop 251 sender from filling up message queue
-		randomNum = 91;			// set up for a new random number
-		while(randomNum % 251 != 0){	//loop until its divisible by 251
-			randomNum = rand();	
-		}
-	} 
+        cout << "Message " << totalMessages << " has been sent, value sent: " << randomNumber << endl;
+				
+	}
+    
+    // send terminating message to sender997
+    msg.mtype = 702;
+    strcpy(msg.greeting, "Terminated receiver1");
+    msgsnd(qid, (struct msgbuf *)&msg, size, 0);
+    
+    // Terminate sender251
 	cout << getpid() << " (Sender251): now exits" << endl;
 	exit(0);
 }
